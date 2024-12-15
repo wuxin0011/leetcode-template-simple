896fd3b566d2b1be156fc4ce0a662c2be099fc7c
wuxin0011 <2191377759@qq.com>
2024-12-15 18:55:12 +0800

    init

---
+++ main/java/everyday/LCDayTemplate.java
@@ -0,0 +1,19 @@
+package everyday;
+
+import code_generation.crwal.leetcode.LCEveryDay;
+
+
+/**
+ * @author: wuxin0011
+ * @Description: 自动获取每日一题 ， 执行该函数会自动拉去 今天的 《每日一题》
+ */
+public class LCDayTemplate {
+
+
+
+    public static void main(String[] args) {
+        LCEveryDay.everyDay.start(LCDayTemplate.class);
+    }
+
+
+}
---
+++ main/java/everyday/readme.md
@@ -0,0 +1,12 @@
+### 每日一题部分
+
+类名命名规则
+code+序号+题目对应序号
+例如 `Code_0001_103`该内容下第一个题目，原题序号为`103`
+
+
+### 模板生成
+
+- [LCDayTemplate](./LCDayTemplate.java)  自动获取每日一题模板
+
+- [LCDailyCustom](./LCDailyCustom.java) 通过输入链接生成每日一题模板 ,有时候忘记了每日一题，可以使用该类
